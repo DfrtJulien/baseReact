6ed961f149deddcd985a02e016f059ecdd4d95e2
DfrtJulien <de.freitas.julien78@gmail.com>
2025-01-25 15:38:27 +0100

    hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 
-const title = "Bonjour les gens";
-const style = { color: "red", backgroundColor: "blue" }
-const showTitle = true
-const todos = [
-  "Manger",
-  "Faire du sport",
-  "Dormir"
-]
 function App() {
 
-  const handleClick = () => {
-    alert("J'ai cliquer sur le titre")
+  const [person, setPerson] = useState({
+    firstName: "Jhon",
+    lastName: "Doe",
+    age: 18
+  });
+
+  const [count, setCount] = useState(0)
+
+  const incrementAge = () => {
+    setPerson({ ...person, age: person.age + 1 })
+  }
+
+  const incrementCount = () => {
+    setCount(count + 1)
   }
 
   return <>
-    {/* <h1 onClick={handleClick} style={style}>{title}</h1> */}
-    {/* si showTitle est true alors j'affiche le h1 */}
-    {/* {showTitle && <h1 style={style}>{title}</h1>}
-    <input type="text" /> */}
-    <Title color="red" id="monid" className="maclass" data-demo="demo">Mon Composant</Title>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt distinctio autem totam praesentium id inventore. Fuga eius tempora, aut consectetur rerum dolore architecto totam sit laudantium exercitationem delectus, beatae id vitae repudiandae! Soluta odit perferendis aliquam. Voluptatem saepe blanditiis quidem ab voluptates libero provident, sapiente impedit dolores porro doloremque similique?</p>
-    <ul>
-      {todos.map(todo => (<li key={todo}>{todo}</li>))}
-    </ul>
+    <p>Age de {person.firstName}: {person.age}</p>
+    <button onClick={incrementAge}>Gagner une année</button>
+    <button onClick={incrementCount}>Incrementé {count}</button>
   </>
 }
 
-function Title({ color, ...props }) {
-
-
-  return <h1 style={{ color: color }} {...props} />
-}
 
 export default App
